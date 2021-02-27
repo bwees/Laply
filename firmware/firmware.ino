4582876a971fc216f697d7f00ea424372f84cdeb
bwees <brandonwees@gmail.com>
2021-02-27 10:10:38 -0500

    cleaned up firmware and documented

--- firmware/firmware.ino
+++ firmware/firmware.ino
@@ -4,8 +4,6 @@
 
 // ************SETTINGS************ //
 
-// EXAMPLE CONFIGS IN THE configs/ DIRECTORY!
-
 // List of channels to handle for this device (array with values ranging from 0-3)
 // Change which channel set is used for each device you upload the firmware to
 // Example, upload {0,1} to your first device and {2,3} to your second
@@ -15,13 +13,13 @@
 // 2 Devices = {0,1} and {2,3}
 // 4 Devices = {0}, {1}, {2}, {3}
 
-int channels[] = {2,3};
+int channels[] = {0,1,2,3};
 
 // Multidevice: Set to 1  if the device will be connected over USB and will be the Master. 
 // Set to 2,3,4 etc. if the device will be a slave.  
 
 // Single Device: Set this to 1. 
-int deviceID = 2;
+int deviceID = 1;
 
 // Pins
 #define PIN_SPI_DATA 10
@@ -38,31 +36,33 @@ int pilotFrequencies[4] = {5658, 5732, 5806, 5880};
 int numChannels = sizeof(channels)/sizeof(channels[0]);
 
 void setup() {
-  // Enable serial if the device is a master
+  // put your setup code here, to run once:
   if (deviceID == 1) {
       Serial.begin(115200);
   }
   
+  sendMessage("DEBUG: SERIAL ACTIVATED");
   rx5808.init();
+  
   Wire.begin(deviceID);
   Wire.onReceive(receiveData);
 }
 
 void loop() {
   // Command Handling
-  if (deviceID == 1) { // if device is master, process serial commands
+  if (deviceID == 1) {
     if (Serial.available() > 0) {
       String command = Serial.readStringUntil('\n');
   
       if (command.indexOf("SETFREQ") >= 0) {
-        distributeToSlaves(command); // distribute message to slaves
-
-        // set frequencies
+        distributeToSlaves(command);
         int pilot = getValue(command, ':', 1).toInt();
         pilotFrequencies[pilot] = getValue(command, ':', 2).toInt();
+        sendMessage(String(deviceID) + "-SUCCESS-" + command);
       }
     }
   }
+  
   rssiDump();
 }
 
@@ -72,7 +72,7 @@ void rssiDump(void) {
     rx5808.setFrequency(pilotFrequencies[channels[i]]);
     delay(35); // Tuning time
 
-    String msg = "RSSI_"; // data output creation
+    String msg = "RSSI_";
     msg += channels[i];
     msg += "-";
     msg += rx5808.readRssi();
@@ -82,9 +82,9 @@ void rssiDump(void) {
 }
 
 void sendMessage(String msg) {
-  if (deviceID == 1) { // if master, output to serial
+  if (deviceID == 1) {
       Serial.println(msg);      
-  } else { // otherwise send to master for output
+  } else {
       Wire.beginTransmission(1);
       Wire.write(msg.c_str());
       Wire.endTransmission();
@@ -92,7 +92,7 @@ void sendMessage(String msg) {
 }
 
 void distributeToSlaves(String msg) {
-  for (int i=2;i<=4; i++) { // distribute to up to 4 slaves
+  for (int i=2;i<=2; i++) {
      Wire.beginTransmission(i);
      Wire.write(msg.c_str());
      Wire.endTransmission();
@@ -100,20 +100,24 @@ void distributeToSlaves(String msg) {
 }
 
 void receiveData(int data) {
-  String command; //command varaible to concat to
+  String command = "";
 
-  while(Wire.available() > 0) // get string from i2c bus
+  while(Wire.available() > 0) // loop through all but the last
   {
     char c = Wire.read(); // receive byte as a character
-    command.concat(c); // add char to string
+    command.concat(c);
   }
 
-  if (command.indexOf("SETFREQ") >= 0) {
+  if (command.indexOf("SETFREQ") >= 0 && command.indexOf("SUCCESS") == -1) {
+    if (deviceID == 1) {
+      distributeToSlaves(command);
+    }
     int pilot = getValue(command, ':', 1).toInt();
     pilotFrequencies[pilot] = getValue(command, ':', 2).toInt();
+    //sendMessage(String(deviceID) + "-SUCCESS-" + command);
   } 
   
-  if (deviceID == 1) { // output received data to serial if master
+  if (deviceID == 1) {
     Serial.println(command);
   }
 }
